5d951c7450a74de75fd85249a8d54c573e0db41d
aleajane <heyya.its.alea@gmail.com>
2018-07-25 18:49:03 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,14 +1,3 @@
-#write out your code here
-
-
-
-def least_coins(cents)
-cents = {"quarters"=> 2, "dimes" => 5, "nickels" => 10, "pennies"=> 50}
-  puts "number = #{cents}"
-end
-
-least_coins(cents)
-
 
 
 def least_coins(cents)
@@ -17,6 +6,6 @@ def least_coins(cents)
  solution[:quarters]=num_quarters
 end 
   
-  
+  least_coins(50)
   
   
