dea8ad3876fc00413c54f000535c04e198a064df
aleajane <heyya.its.alea@gmail.com>
2018-07-25 18:29:09 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,6 +3,6 @@
 
 
 def least_coins(cents)
-50 =>{"quarters"=> 2, "dimes" => 5, "nickels" => 10 "pennies"=> 50}
+50 => {"quarters"=> 2, "dimes" => 5, "nickels" => 10 "pennies"=> 50}
   puts "number = #{coins}"
 end
