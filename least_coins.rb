e95142420353bbb70453db11715bb21f064dbe42
aleajane <heyya.its.alea@gmail.com>
2018-07-25 18:57:40 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -8,6 +8,16 @@ def least_coins(cents)
  
 num_dimes = cents/ 10
  solution[:dimes]=num_dimes
+ cents = cents - 10*num_dimes
+ 
+ num_nickels = cents/5 
+ solution[:nickels]= num_nickels
+ cents = cents - 5*num_nickels
+ 
+  num_pennies = cents/1
+  solution [:pennies]= num_pennies
+  cents = cents - 1*num_nickels
+ 
  puts solution
 end 
   
