9d65889b276dbf1580f9e52b83a0948942a49dd3
aleajane <heyya.its.alea@gmail.com>
2018-07-25 18:28:11 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,6 +4,6 @@
 
 def least_coins(cents)
   number = 50 
-coins={"quarters"=> 2, "dimes" => 5, "nickels" => 10 "pennies"=> 50}
+50={"quarters"=> 2, "dimes" => 5, "nickels" => 10 "pennies"=> 50}
   puts "number = #{coins}"
 end
