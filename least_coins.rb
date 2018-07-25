727549cdf9b873a2e4dfa9a76281592c25e294b5
aleajane <heyya.its.alea@gmail.com>
2018-07-25 18:51:55 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,7 +4,7 @@ def least_coins(cents)
   solution = {}
   num_quarters = cents / 25  
  solution[:quarters]=num_quarters
- num_dimes =cents / 10 
+ num_dimes =cents / 11
  solution[:dimes]=num_dimes
  puts solution
 end 
